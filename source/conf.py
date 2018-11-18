d802746c90574d7ba5cd10f9f14279bd604d1a25
Rick Casady <richard.casady@gmail.com>
2018-11-17 21:09:18 -0800

    update intro

--- source/conf.py
+++ source/conf.py
@@ -68,7 +68,6 @@ exclude_patterns = []
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
